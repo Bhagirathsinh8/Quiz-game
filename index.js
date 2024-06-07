9804be1c4b9fee97f1126bd2dfe0859ff42d4086
Bhagirathsinh8 <122227146+Bhagirathsinh8@users.noreply.github.com>
2024-06-08 02:20:47 +0530

    API update

--- index.js
+++ index.js
@@ -2,26 +2,65 @@ const quizDB = [
     {
         question: "Inside which HTML element do we put the JavaScript??",
         option1: "<script>",
-        "choice2": "<javascript>",
-        "choice3": "<js>",
-        "choice4": "<scripting>",
-        "answer": "ans1",
+        option2: "<javascript>",
+        option3: "<js>",
+        option4: "<scripting>",
+        answers: "ans1",
     },
     {
-        "question":
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        "choice1": "<script href='xxx.js'>",
-        "choice2": "<script name='xxx.js'>",
-        "choice3": "<script src='xxx.js'>",
-        "choice4": "<script file='xxx.js'>",
-        "answer": 3,
+        question:    "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        option1: "<script href='xxx.js'>",
+        option2: "<script name='xxx.js'>",
+        option3: "<script src='xxx.js'>",
+        option4: "<script file='xxx.js'>",
+        answers: "ans3",
     },
     {
-        "question": " How do you write 'Hello World' in an alert box?",
-        "choice1": "msgBox('Hello World');",
-        "choice2": "alertBox('Hello World');",
-        "choice3": "msg('Hello World');",
-        "choice4": "alert('Hello World');",
-        "answer": 4,
+        question: " How do you write 'Hello World' in an alert box?",
+        option1: "msgBox('Hello World');",
+        option2: "alertBox('Hello World');",
+        option3: "msg('Hello World');",
+        option4: "alert('Hello World');",
+        answers: "ans4",
     }
-]
+];
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit = document.querySelector('#submit');
+const answers = document.querySelectorAll('.answer')
+
+let questionCount = 0;
+const loadQuestion = () =>{
+    const questionList = quizDB[questionCount];
+
+    question.innerText = questionList.question;
+    option1.innerText = questionList.option1;
+    option2.innerText = questionList.option2;
+    option3.innerText = questionList.option3;
+    option4.innerText = questionList.option4;
+    
+}
+
+loadQuestion();
+
+submit.addEventListener('click',()=>{
+    const checkedAnswer = getAnswert();
+    console.log(checkedAnswer);
+})
+
+const getAnswert = () =>{
+    let answer;  
+    answers.forEach((currentAns) =>{
+        if(currentAns.checked){
+            answer = currentAns.id;
+         }
+      
+    });
+    return answer;
+}
+
+
